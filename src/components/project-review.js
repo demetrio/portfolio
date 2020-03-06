70b54d39cfb09c8d0e40463a1de824c5d9ff8add
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2020-03-06 23:16:41 +0100

    Adds project review component with projects

--- src/components/project-review.js
+++ src/components/project-review.js
@@ -1,20 +1,48 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Image from 'gatsby-image';
+import styled from '@emotion/styled';
 
-const ProjectPreview = ({ url, title, description, imageData }) => (
-	<div className="project-preview">
-		<Link to={`/${url}/`}>
+const Project = styled.div`
+	* {
+		box-sizing: border-box;
+	}
+
+	box-shadow: 0 3px 15px rgba(51, 51, 51, 0.2);
+	border-radius: 10px;
+	overflow: auto;
+
+	.image {
+		border-bottom: 7px solid #673ab7;
+	}
+
+	.info {
+		padding: 1em 1em 0;
+		background-color: red;
+
+		h3 {
+			margin-bottom: 10px;
+		}
+	}
+`;
+
+const ProjectPreview = ({ url, title, description, imageData, git_url, tags }) => (
+	<Project>
+		<div className="image">
 			<Image fluid={imageData} alt={title} />
-			<h2>
-				<Link to={`/${url}/`}>{url}</Link>
-			</h2>
+		</div>
+
+		<div className="info">
+			<h3>{title}</h3>
 			<p>{description}</p>
-			<p>
-				<Link to={`/${url}/`}>View this project</Link>
-			</p>
-		</Link>
-	</div>
+		</div>
+		<div className="url">
+			<p>{url}</p>
+			<p>{git_url}</p>
+		</div>
+		<div className="url">
+			<p>{tags.map(tag => tag)}</p>
+		</div>
+	</Project>
 );
 
 export default ProjectPreview;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,6 +3,13 @@ import { useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import ProjectPreview from '../components/project-review';
+import styled from '@emotion/styled';
+
+const ListProjects = styled.div`
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	grid-gap: 1em;
+`;
 
 const Projects = () => {
 	const data = useStaticQuery(graphql`
@@ -14,6 +21,8 @@ const Projects = () => {
 						title
 						description
 						url
+						git_url
+						tags
 						image {
 							childImageSharp {
 								fluid {
@@ -31,12 +40,23 @@ const Projects = () => {
 
 	return (
 		<Layout>
-			{projects.map(({ node: project }) => {
-				const { id, title, description, url } = project;
-				const imageData = project.image.childImageSharp.fluid;
-				return <ProjectPreview key={id} title={title} description={description} imageData={imageData} url={url} />;
-			})}
-
+			<ListProjects>
+				{projects.map(({ node: project }) => {
+					const { id, title, description, url, git_url, tags } = project;
+					const imageData = project.image.childImageSharp.fluid;
+					return (
+						<ProjectPreview
+							key={id}
+							title={title}
+							description={description}
+							imageData={imageData}
+							url={url}
+							git_url={git_url}
+							tags={tags}
+						/>
+					);
+				})}
+			</ListProjects>
 			<SEO title="Projects" />
 		</Layout>
 	);
