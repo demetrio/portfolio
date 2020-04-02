d98e63da62726681ab3b66ecbd1e20f36b0cba44
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2020-04-02 19:40:22 +0200

    Updated manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,8 +35,8 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
-				name: `gatsby-starter-default`,
-				short_name: `starter`,
+				name: `Demetrio.dev`,
+				short_name: `demetrio`,
 				start_url: `/`,
 				background_color: `#663399`,
 				theme_color: `#663399`,
