763feaaf99bc0b84268bb378430c1ce39efc0a63
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2020-08-10 00:21:36 +0200

    Refactor Help component
    Moved the Styled Component to its own file

--- src/components/help.js
+++ src/components/help.js
@@ -1,17 +1,9 @@
 import React, { useState } from 'react';
-import styled from '@emotion/styled';
+import { HelpButton } from './help.style';
 
 const Help = () => {
 	const [help, setHelps] = useState(0);
 	const label = ` ${help % 2 === 0 ? '🙂' : '🙃'} Hey! You clicked ${help} ${help === 1 ? 'time' : 'times'}!!`;
-
-	const HelpButton = styled.button`
-		background: rebeccapurple;
-		border: none;
-		color: white;
-		padding: 0.75rem 1rem;
-		font-size: 1.25rem;
-	`;
 
 	return <HelpButton onClick={() => setHelps(help + 1)}>{label}</HelpButton>;
 };
