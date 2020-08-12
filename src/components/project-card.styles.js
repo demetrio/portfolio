75d28e82f738e9fc8d0ddcc7a233ed7ec2b913a9
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2020-08-12 02:22:04 +0200

    Changed Project Card Styles

--- src/components/project-card.styles.js
+++ src/components/project-card.styles.js
@@ -3,8 +3,8 @@ import colors from '../styles/colors';
 
 export const Project = styled.div`
 	box-sizing: border-box;
-	box-shadow: 0 3px 15px rgba(51, 51, 51, 0.2);
-	border-radius: 10px;
+	box-shadow: rgba(50, 50, 50, 0.15) 0 0 5px 1px;
+	border-radius: 5px;
 	overflow: auto;
 `;
 
@@ -39,6 +39,7 @@ export const Tag = styled.span`
 	border: 1px solid black;
 	text-align: center;
 	padding: 5px 10px;
+	border-radius: 5px;
 `;
 
 export const Button = styled.a`
